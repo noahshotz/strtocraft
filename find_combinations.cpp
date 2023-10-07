4c98c492522f55deb782f652714a722ab53950ae
noahshotz <max.heinrich@uusimaa.de>
2023-10-07 16:59:20 +0200

    first running demo

--- find_combinations.cpp
+++ find_combinations.cpp
@@ -1,45 +1,99 @@
 #include <iostream>
 #include <map>
 #include <vector>
-using namespace std;
+#include <string>
+#include <algorithm>
 
-void find_combinations(string input_str) {
-
-    // define dictionary for subtrings and corresponding stickers
-    map<string, string> dictionary = {
-        {"A", "Apeks"},         {"B", "b_placeholder"},
-        {"C", "c_placeholder"}, {"D", "d_placeholder"},
-        {"E", "e_placeholder"}, {"F", "f_placeholder"},
-        {"G", "g_placeholder"}, {"H", "h_placeholder"}, 
-        {"I", "i_placeholder"}, {"J", "j_placeholder"}, 
-        {"K", "k_placeholder"}, {"L", "l_placeholder"}, 
-        {"M", "m_placeholder"}, {"N", "n_placeholder"}, 
-        {"O", "o_placeholder"}, {"P", "p_placeholder"}, 
-        {"Q", "q_placeholder"}, {"R", "reason"}, 
-        {"S", "s_placeholder"}, {"T", "t_placeholder"}, 
-        {"U", "u_placeholder"}, {"V", "v_placeholder"}, 
-        {"W", "w_placeholder"}, {"X", "x_placeholder"}, 
-        {"Y", "y_placeholder"}, {"Z", "z_placeholder"}, 
-    };
-
+std::vector<std::string> find_combinations(const std::string &input_str, std::map<std::string, std::string> dictionary)
+{
     // Create a vector to store the best combinations for each position in the input string
-    // Initialize it with an empty vector for the first 4 positions and nullptr for the rest
-    // This is limited to 4 positions since most weapons in CS:GO/CS2 only offer 4 slots for stickers
-    vector<vector<string>> dp(min(4, static_cast<int>(input_str.length())));
-    dp.insert(dp.end(), input_str.length() - 4, vector<string>());
+    std::vector<std::vector<std::string>> dp(input_str.length() + 1);
 
-    for(char c : input_str) {
-        cout << "char: " << c << endl;
+    dp[0].emplace_back(""); // Add an empty string at position 0
 
-    };
+    for (int i = 1; i <= static_cast<int>(input_str.length()); ++i)
+    {
+        std::vector<std::string> current_combinations;
 
-};
+        for (int j = 1; j <= i; ++j)
+        {
+            std::string substring = input_str.substr(j - 1, i - j + 1);
 
-// Variable declaration at the global scope
-string input_str = "GLASA";
+            if (dictionary.find(substring) != dictionary.end())
+            {
+                std::string sticker = dictionary[substring];
 
-int main() {
-    // Call the function
-    find_combinations(input_str);
+                for (const std::string &prev_combination : dp[j - 1])
+                {
+                    current_combinations.push_back(prev_combination + (prev_combination.empty() ? "" : " ") + sticker);
+                }
+            }
+        }
+
+        std::sort(current_combinations.begin(), current_combinations.end(), [](const std::string &a, const std::string &b)
+                  { return a.size() < b.size(); });
+
+        dp[i] = (current_combinations.size() <= 4) ? current_combinations : std::vector<std::string>();
+    }
+
+    return dp.back(); // Return the computed combinations
+}
+
+int main()
+{
+    // Define dictionary for substrings and corresponding stickers
+    std::map<std::string, std::string> dictionary = {
+        {"A", "Apeks"},
+        {"B", "Flipsid3"},
+        {"C", "London Conspiracy"},
+        {"D", "d_placeholder"},
+        {"E", "e_placeholder"},
+        {"F", "Faze"},
+        {"G", "LGB"},
+        {"H", "Heroic"},
+        {"I", "Flashbang"},
+        {"J", "j_placeholder"},
+        {"K", "SK Gaming"},
+        {"L", "l_placeholder"},
+        {"M", "Clan-Mystik"},
+        {"N", "Team EnvyUs"},
+        {"O", "Evil Geniuses"},
+        {"P", "Pain Gaming"},
+        {"Q", "q_placeholder"},
+        {"R", "Reason Gaming"},
+        {"S", "Splyce"},
+        {"T", "Titan"},
+        {"U", "u_placeholder"},
+        {"V", "Vitality"},
+        {"W", "w_placeholder"},
+        {"X", "x_placeholder"},
+        {"Y", "y_placeholder"},
+        {"Z", "z_placeholder"},
+        {"GL", "GamerLegion"},
+        {"GG", "Good Game"},
+        {"EZ", "EZ"},
+        {"REKT", "REKT"},
+        {"RUSH", "RUSH"},
+        {"PEEK", "PEEK"},
+        {"GO", "GO"},
+        {"BIG", "BIG"},
+        {"DICK", "DICK"},
+        {"IM", "iM"}};
+
+    // Word to convert to sticker craft
+    std::string input_str = "NOAH";
+
+    std::cout << "Finding combinations for: " << input_str << std::endl;
+
+    std::vector<std::string> combinations = find_combinations(input_str, dictionary);
+
+    std::cout << "Found " << combinations.size() << " possible combinations: " << std::endl;
+
+    // Print the combinations
+    for (const std::string &combination : combinations)
+    {
+        std::cout << combination << std::endl;
+    }
+
     return 0;
 }
