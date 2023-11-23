ef160c5a5fdc2e1669e762c9325ce26fe5bffd72
noahshotz <max.heinrich@uusimaa.de>
2023-11-23 12:26:54 +0100

    updated logic to not exceed 4 stickers per combination

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include "src/dictionary.cpp"
 
 int main(int, char**){
-    std::cout << "Hello, from strtocraft!\n";
 
     std::string input_str = "GLASA";
 
     std::vector<std::string> combinations = find_combinations(input_str, dictionary);
 
+    std::cout << "Input: " << input_str << std::endl;
+
+    std::size_t i = 1;
     for (auto combination : combinations) {
-        std::cout << combination << std::endl;
+        std::cout << "Option " << i << ": " << combination << std::endl;
+        i++;
     }
 }
--- src/find_combinations.cpp
+++ src/find_combinations.cpp
@@ -8,23 +8,39 @@ std::vector<std::string> find_combinations(const std::string &input_str, std::ma
 {
     // Create a vector to store the best combinations for each position in the input string
     std::vector<std::vector<std::string>> dp(input_str.length() + 1);
+
     // Add an empty string at position 0
     dp[0].emplace_back("");
 
+    // Set limit for the number of stickers in a combination
+    const std::size_t max_elements = 4;
+
     for (int i = 1; i <= static_cast<int>(input_str.length()); ++i)
     {
+        // Create a vector to store the current combinations
         std::vector<std::string> current_combinations;
         for (int j = 1; j <= i; ++j)
         {
+            // Get the substring from j to i
             std::string substring = input_str.substr(j - 1, i - j + 1);
 
+            // Check if the substring is in the dictionary
             if (dictionary.find(substring) != dictionary.end())
             {
+                // Get the sticker from the dictionary
                 std::string sticker = dictionary[substring];
 
+                // Add the sticker to the current combinations
                 for (const std::string &prev_combination : dp[j - 1])
                 {
-                    current_combinations.push_back(prev_combination + (prev_combination.empty() ? "" : ", ") + sticker);
+                    // Count the number of elements in prev_combination
+                    std::size_t element_count = std::count(prev_combination.begin(), prev_combination.end(), ',') + 1;
+
+                    // Check if adding a new element exceeds the limit
+                    if (element_count < max_elements)
+                    {
+                        current_combinations.push_back(prev_combination + (prev_combination.empty() ? "" : ", ") + sticker);
+                    }
                 }
             }
         }
